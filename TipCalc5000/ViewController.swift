98fefa7457013064435971c36acccdd66fef75b0
Cody Evol <cody@evolcreative.com>
2015-10-09 13:04:32 -0600

    Initial Commit

---
+++ TipCalc5000/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalc5000
+//
+//  Created by Cody Evol on 10/9/15.
+//  Copyright © 2015 Cody Evol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
